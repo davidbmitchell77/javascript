7605df0ca6935103a9e9e962f64c15efb9e84a7a
David Mitchell <davidbmitchell77@outlook.com>
2022-03-02 13:04:34 -0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -130,10 +130,15 @@ export default
             let year = ((d) ? d : new Date()).getFullYear();
             if ((year % 4) == 0)
             {
+		/*
                 let exceptions = new Set([ 1900, 2100, 2200, 2300, 2500, 2700, 2900, 3000, 3100 ]);
                 if (!exceptions.has(year)) {
                     result = true;
                 }
+		*/
+		if ((year % 100) != 0) || (year % 400) == 0)) {
+                    result = true;
+		}
             }
 
             return result;
